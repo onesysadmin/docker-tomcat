f51bb969412ca4679c404bccf55fd69b84772156
Alex Sanz <asans@evirtualpost.com>
2016-02-02 15:49:21 -0800

    support for SERVICE_NAME to change context
    if SERVICE_NAME env is set, then the script will rename ROOT/ or
    ROOT.war in the webapps/ directory to the new SERVICE_NAME.  This allows
    us to add servlet context path to the webapp.

--- 8/run.sh
+++ 8/run.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+WEBAPPS_DIR=${CATALINA_HOME}/webapps
+
+# rename ROOT.war and ROOT/ to SERVICE_NAME if it exists
+# this will force the ROOT app to be under a context path
+if [ ! -z "$SERVICE_NAME" ]; then
+  rm -rf ${WEBAPPS_DIR}/${SERVICE_NAME} ${WEBAPPS_DIR}/${SERVICE_NAME}.war
+	if [ -d "${WEBAPPS_DIR}/ROOT" ]; then
+		mv ${WEBAPPS_DIR}/ROOT ${WEBAPPS_DIR}/${SERVICE_NAME}
+		echo "Renaming ROOT directory to ${SERVICE_NAME}"
+	fi
+	if [ -f "${WEBAPPS_DIR}/ROOT.war" ]; then
+		mv ${WEBAPPS_DIR}/ROOT.war ${WEBAPPS_DIR}/${SERVICE_NAME}.war
+		echo "Renaming ROOT.war to ${SERVICE_NAME}.war"
+	fi
+fi
+
 if [ ! -f /.tomcat_admin_created ]; then
     /create_tomcat_admin_user.sh
 fi
